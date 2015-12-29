dbcc8d29f08bbc896919f93af3f3b9f33f212312
Rocky Duan <dementrock@gmail.com>
2015-12-29 18:14:36 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='tensorfuse',
-      version='0.1',
-      description='Common interface for Theano, CGT, and TensorFuse',
+      version='0.0.1',
+      description='Common interface for Theano, CGT, and TensorFlow',
       url='http://github.com/dementrock/tensorfuse',
       author='Rocky Duan',
       author_email='dementrock@gmail.com',
